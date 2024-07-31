1e1b7af7eb5db456df4fbfb9e9f31deb517fa414
priti <wadatkarpriti454@gmail.com>
2024-07-31 20:14:32 +0530

    academics data

--- resources/views/pages/academics/school-best-practice.blade.php
+++ resources/views/pages/academics/school-best-practice.blade.php
@@ -2,4 +2,127 @@
 
 @section('main-content')
 
+<!-- Page Header Start here -->
+<section class="page-header section-notch">
+<div class="overlay">
+<div class="container">
+<h3>School Best Practice</h3>
+    <ul>
+        <li><a href="../../index.html">Home /</a></li>
+        <li>Academics/ School Best Practice</li>
+    </ul>
+</div><!-- container -->
+</div><!-- overlay -->
+</section><!-- page header -->
+<!-- Page Header End here -->
+<!-- Blog Post Start here -->
+<section class="blog-post padding-120">
+<div class="container">
+<div class="row">
+<div class="col-md-9 post-item-pagination">
+<div class="post-items">
+<div class="post-item">
+<div class="post-content">
+     <h3><a href="#">Academic Management</a></h3>
+<p>Academic Management in BPSN is done by using resources to achieve educational goal.</p>
+
+<p><strong>Teaching learning process&nbsp;</strong>is monitored on daily basis, Teachers are masters in their subject &amp; are professionally trained. They try to meet expectation of the society by using updated teaching methodologies.</p>
+
+<p><strong>Teachers undergo training&nbsp;</strong>to keep abreast with new knowledge practices. Teachers communicate with parents about progress of their child.</p>
+
+<p><strong>Time management&nbsp;</strong>is a key to success for academic progress various curricular activities are carried out in the school to develop reasoning, psychomotor &amp; emotional learning. Teachers work hard to strike balance between syllabus, curricular books &amp; co-curricular activities.</p>
+
+<p>All round development essentially includes intellectual, moral &amp; social development. School strives to achieve it by good academic management.</p>
+ </div>
+</div><!-- post item -->
+</div><!-- post items -->
+</div>
+ <div class="col-md-3">
+                <div class="sidebar">
+
+                        <div class="sidebar-item">
+                            <h3 class="sidebar-title">Academics</h3>
+
+                            <ul class="sidebar-categories">
+                                                                           <li><a
+                                                href="#">Curriculum Details</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Learning Resources</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">School Best Practice</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Academic Calendar</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">BPSN Newsletter</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Environmental Education</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Student Services</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Book List</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Holiday List</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Student Strength 2024-2025</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Teachers Training Details</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Alumni Association</a>
+                                        </li>
+                             </ul>
+                        </div>
+
+                        <div class="sidebar-item">
+                            <h3 class="sidebar-title">All Activities</h3>
+
+                            <ul class="sidebar-categories">
+                                                                              <li><a
+                                                href="#">Co-Curricular Activities</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Extra-Curricular Activities</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Celebrations</a>
+                                        </li>
+                             </ul>
+                        </div>
+
+                        <div class="sidebar-item">
+                            <h3 class="sidebar-title">All Achievements</h3>
+
+                            <ul class="sidebar-categories">
+                                                                        <li><a
+                                                href="#">School Achievements</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Faculty Achievements</a>
+                                        </li>
+                                                                            <li><a
+                                                href="#">Student Achievements</a>
+                                        </li>
+                             </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Post End here -->
+
+
+
 @endsection
+
