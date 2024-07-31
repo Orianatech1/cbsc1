6f472a6dab52b5b34e8538fe1a28e75f3ad3f0ef
OrianaTech <165065778+Orianatech1@users.noreply.github.com>
2024-07-31 18:43:25 +0530

    acadamic pages added

--- resources/views/pages/academics/academic-achievements.blade.php
+++ resources/views/pages/academics/academic-achievements.blade.php
@@ -2,4 +2,229 @@
 
 @section('main-content')
 
+
+
+<!-- Page Header Start here -->
+<section class="page-header section-notch">
+    <div class="overlay">
+        <div class="container">
+        <h3>School Achievements</h3>
+        <ul>
+            <li><a href="../../index.html">Home /</a></li>
+            <li>Academics / School Achievements</li>
+        </ul>
+        </div><!-- container -->
+    </div><!-- overlay -->
+    </section><!-- page header -->
+    <!-- Page Header End here -->
+
+                    <!-- Product start here -->
+      <section class="product padding-120">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-9 col-xs-12 post-item-pagination">
+
+              <div class="product-items">
+
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct1">
+                                                                                  <img src="../../storage/Menus/Activity/118/118/19bd21f73d444489a87080d8ee5ceb5b.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="118/edit.html">STD 10 Result 2024</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct2">
+                                                                                  <img src="../../storage/Menus/Activity/1/117/1630a9dd59c7497890b10fed8e03a5f8.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="117/edit.html">STD 12 Result 2024</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct3">
+                                                                                  <img src="../../storage/Menus/Activity/1/114/IMG-20240130-WA0009.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="114/edit.html">Gold Medal Winners In Olimpiad Enlgish</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct4">
+                                                                                  <img src="../../storage/Menus/Activity/1/113/IMG-20240213-WA0034.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="113/edit.html">Winners of Hooping for Humanity Basketball Tournament</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct5">
+                                                                                  <img src="../../storage/Menus/Activity/1/110/IMG-20231222-WA0015.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="110/edit.html">Shraddha Valhekar</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct6">
+                                                                                  <img src="../../storage/Menus/Activity/1/109/IMG-20230427-WA0068%20(1).jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="109/edit.html">Under 14 boys Winners of SFA Inter-school Basketball Championship</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct7">
+                                                                                  <img src="../../storage/Menus/Activity/1/106/IMG-20231107-WA0002.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="106/edit.html">Winners of Viva Inter-school Basketball tournament</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct8">
+                                                                                  <img src="../../storage/Menus/Activity/1/104/IMG-20231025-WA0010.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="104/edit.html">Vishwajeet Vaingade</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div class="product-item-grid">
+                  <div class="pro-item">
+                    <div class="pro-image cd-item" id="prdct9">
+                                                                                  <img src="../../storage/Menus/Activity/1/101/IMG-20231025-WA0009.jpg" style="height: 260px; width:288px;" alt="product image" class="img-responsive">
+
+                    </div>
+                    <div class="pro-content" style="height: 100px !important;">
+                      <strong><a href="101/edit.html">Shreya kagade Won Silver Medal  In  CBSE Taekwondo Championship</a></strong>
+
+                    </div><!-- pro-content -->
+                  </div> <!-- pro item -->
+                </div><!-- product-item -->
+
+
+                <div id="cd-quick-view-coverlay"></div>
+              </div><!-- product items -->
+
+              <ul class="pagination product-pagination">
+                <ul class="pagination">
+
+                        <li class="disabled"><span>&laquo;</span></li>
+
+
+
+
+
+                                                                            <li class="active"><span>1</span></li>
+                                                                                    <li><a href="14658.html?page=2">2</a></li>
+                                                                                    <li><a href="19ba9.html?page=3">3</a></li>
+                                                                                    <li><a href="1fdb0.html?page=4">4</a></li>
+
+
+                        <li><a href="14658.html?page=2" rel="next">&raquo;</a></li>
+                </ul>
+
+
+              </ul>
+            </div>
+            <div class="col-lg-3 col-xs-12">
+               <div class="sidebar">
+
+                <div class="sidebar-item">
+                  <h3 class="sidebar-title">Academics</h3>
+
+                  <ul class="sidebar-categories">
+                                                                <li><a href="../academic_page_display/1.html">Curriculum Details</a></li>
+                                                <li><a href="../academic_page_display/2.html">Learning Resources</a></li>
+                                                <li><a href="../academic_page_display/3.html">School Best Practice</a></li>
+                                                <li><a href="../academic_page_display/4.html">Academic Calendar</a></li>
+                                                <li><a href="../academic_page_display/13.html">BPSN Newsletter</a></li>
+                                                <li><a href="../academic_page_display/5.html">Environmental Education</a></li>
+                                                <li><a href="../academic_page_display/6.html">Student Services</a></li>
+                                                <li><a href="../academic_page_display/15.html">Book List</a></li>
+                                                <li><a href="../academic_page_display/12.html">Holiday List</a></li>
+                                                <li><a href="../academic_page_display/16.html">Student Strength 2024-2025</a></li>
+                                                <li><a href="../academic_page_display/8.html">Teachers Training Details</a></li>
+                                                <li><a href="../academic_page_display/10.html">Alumni Association</a></li>
+                                                      </ul>
+                </div>
+
+                <div class="sidebar-item">
+                  <h3 class="sidebar-title">All Activities</h3>
+
+                  <ul class="sidebar-categories">
+                                                                <li><a href="../activity_page_display/1.html">Co-Curricular Activities</a></li>
+                                                <li><a href="../activity_page_display/2.html">Extra-Curricular Activities</a></li>
+                                                <li><a href="../activity_page_display/4.html">Celebrations</a></li>
+                                                      </ul>
+                </div>
+
+                <div class="sidebar-item">
+                  <h3 class="sidebar-title">All Achievements</h3>
+
+                  <ul class="sidebar-categories">
+                                                                <li><a href="1.html">School Achievements</a></li>
+                                                <li><a href="2.html">Faculty Achievements</a></li>
+                                                <li><a href="3.html">Student Achievements</a></li>
+                                                      </ul>
+                </div>
+
+
+            </div>
+          </div><!-- row -->
+        </div><!-- container -->
+      </section>
+      <!-- Product end here -->
+
 @endsection
--- resources/views/pages/academics/holiday-list.blade.php
+++ resources/views/pages/academics/holiday-list.blade.php
@@ -2,4 +2,126 @@
 
 @section('main-content')
 
+
+    <!-- Page Header Start here -->
+    <section class="page-header section-notch">
+        <div class="overlay">
+            <div class="container">
+                                    <h3>Holiday List</h3>
+                                <ul>
+                    <li><a href="../../index.html">Home /</a></li>
+                    <li>Academics
+                                                    / Holiday List
+                                            </li>
+                </ul>
+            </div><!-- container -->
+        </div><!-- overlay -->
+    </section><!-- page header -->
+    <!-- Page Header End here -->
+
+
+    <!-- Blog Post Start here -->
+    <section class="blog-post padding-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9 post-item-pagination">
+                                                                        <div class="post-items">
+                                <div class="post-item">
+                                    <div class="post-content">
+                                        <h3><a href="#">Holiday List</a></h3>
+                                                                                    <object data="../../storage/Menus/Academic/27/1717389625List%20of%20Holidays%20for%20the%20Academic%20Year%202024.pdf" type="application/pdf" width="100%"
+                                                height="800px" internalinstanceid="20"></object>
+                                                                                                                            <p>Holiday List</p>
+                                                                            </div>
+                                </div><!-- post item -->
+                            </div><!-- post items -->
+
+
+                </div>
+                <div class="col-md-3">
+                    <div class="sidebar">
+
+                        <div class="sidebar-item">
+                            <h3 class="sidebar-title">Academics</h3>
+
+                            <ul class="sidebar-categories">
+                                                                                                            <li><a
+                                                href="1.html">Curriculum Details</a>
+                                        </li>
+                                                                            <li><a
+                                                href="2.html">Learning Resources</a>
+                                        </li>
+                                                                            <li><a
+                                                href="3.html">School Best Practice</a>
+                                        </li>
+                                                                            <li><a
+                                                href="4.html">Academic Calendar</a>
+                                        </li>
+                                                                            <li><a
+                                                href="13.html">BPSN Newsletter</a>
+                                        </li>
+                                                                            <li><a
+                                                href="5.html">Environmental Education</a>
+                                        </li>
+                                                                            <li><a
+                                                href="6.html">Student Services</a>
+                                        </li>
+                                                                            <li><a
+                                                href="15.html">Book List</a>
+                                        </li>
+                                                                            <li><a
+                                                href="12.html">Holiday List</a>
+                                        </li>
+                                                                            <li><a
+                                                href="16.html">Student Strength 2024-2025</a>
+                                        </li>
+                                                                            <li><a
+                                                href="8.html">Teachers Training Details</a>
+                                        </li>
+                                                                            <li><a
+                                                href="10.html">Alumni Association</a>
+                                        </li>
+                                                                                                </ul>
+                        </div>
+
+                        <div class="sidebar-item">
+                            <h3 class="sidebar-title">All Activities</h3>
+
+                            <ul class="sidebar-categories">
+                                                                                                            <li><a
+                                                href="../activity_page_display/1.html">Co-Curricular Activities</a>
+                                        </li>
+                                                                            <li><a
+                                                href="../activity_page_display/2.html">Extra-Curricular Activities</a>
+                                        </li>
+                                                                            <li><a
+                                                href="../activity_page_display/4.html">Celebrations</a>
+                                        </li>
+                                                                                                </ul>
+                        </div>
+
+                        <div class="sidebar-item">
+                            <h3 class="sidebar-title">All Achievements</h3>
+
+                            <ul class="sidebar-categories">
+                                                                                                            <li><a
+                                                href="../achievement_page_display/1.html">School Achievements</a>
+                                        </li>
+                                                                            <li><a
+                                                href="../achievement_page_display/2.html">Faculty Achievements</a>
+                                        </li>
+                                                                            <li><a
+                                                href="../achievement_page_display/3.html">Student Achievements</a>
+                                        </li>
+                                                                                                </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Post End here -->
+
+
 @endsection
